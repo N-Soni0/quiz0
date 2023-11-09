80536f34c564b6f78d9996091885c3020fbfd316
N-Soni0 <anton.stecyura.pr1@gmail.com>
2023-11-09 16:06:59 +0200

    Theme switching feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
 import { ClerkProvider } from '@clerk/nextjs';
+import { ThemeProvider } from '@/components/theme-provider';
 import { Inter } from 'next/font/google';
+import { Header } from '@/components/header';
 import './globals.css';
-import Header from '@/components/header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,10 +21,17 @@ export default function RootLayout({
 		<ClerkProvider>
 			<html lang='en'>
 				<body
-					className={`min-h-screen  h-screen dark bg-background flex flex-col overflow-hidden ${inter.className}`}
+					className={`min-h-screen  h-screen bg-background flex flex-col overflow-hidden ${inter.className}`}
 				>
-					<Header />
-					{children}
+					<ThemeProvider
+						attribute='class'
+						defaultTheme='system'
+						enableSystem
+						disableTransitionOnChange
+					>
+						<Header />
+						{children}
+					</ThemeProvider>
 				</body>
 			</html>
 		</ClerkProvider>
