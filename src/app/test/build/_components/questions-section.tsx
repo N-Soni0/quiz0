2b084b23ce68d114fd13ac40d8a428fd60de65ac
N-Soni0 <anton.stecyura.pr1@gmail.com>
2023-11-19 00:40:46 +0200

    Extracted questions section from test form

---
+++ src/app/test/build/_components/questions-section.tsx
@@ -0,0 +1,64 @@
+import { Button } from '@/components/ui/button';
+import { TestFormData } from '../schema';
+import QuestionDialog from './question-dialog';
+import { QUESTIONS_DISPLAY_TEXT } from '@/constants/questions';
+
+type Props = {
+	questions: TestFormData['questions'];
+};
+
+const QuestionsSection = ({ questions }: Props) => {
+	return (
+		<div className='w-full flex justify-center items-center flex-col'>
+			<QuestionDialog index={questions.length}>
+				<Button
+					variant={'secondary'}
+					className='w-[30%] block mb-5'
+				>
+					Add
+				</Button>
+			</QuestionDialog>
+
+			{questions.length === 0 ? (
+				<div className='w-full py-2 px-4 flex items-center justify-center bg-muted/50 rounded-lg'>
+					No questions yet
+				</div>
+			) : (
+				<div className='w-full grid grid-cols-2 gap-3 mb-3'>
+					{questions.map((question, index) => (
+						<QuestionDialog
+							key={index}
+							index={index}
+							initialValues={question}
+						>
+							<Button
+								variant={'outline'}
+								className='w-full z-0 relative h-fit border-2'
+							>
+								<div className='w-full text-left '>
+									<div className='mb-2 '>
+										<h4 className='w-full text-lg font-bold'>
+											Question №{index + 1}
+										</h4>
+
+										<p className='text-primary/70'>
+											{QUESTIONS_DISPLAY_TEXT[question.type]}
+										</p>
+									</div>
+
+									<div className='w-full justify-between gap-3 items-center'>
+										<p className='whitespace-normal h-10 break-all line-clamp-2'>
+											{question.text}
+										</p>
+									</div>
+								</div>
+							</Button>
+						</QuestionDialog>
+					))}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default QuestionsSection;
--- src/app/test/build/_components/test-form.tsx
+++ src/app/test/build/_components/test-form.tsx
@@ -95,47 +95,16 @@ const TestForm = () => {
 							/>
 						</div>
 
-						<div className='w-full flex justify-center items-center flex-col'>
-							<div className='w-full grid grid-cols-2 gap-3 mb-3'>
-								{questions.map((question, index) => (
-									<QuestionDialog
-										key={index}
-										index={index}
-										initialValues={question}
-									>
-										<Button
-											variant={'outline'}
-											className='w-full z-0 relative h-fit border-2'
-										>
-											<div className='w-full text-left '>
-												<div className='mb-2 '>
-													<h4 className='w-full text-lg font-bold'>
-														Question №{index + 1}
-													</h4>
-
-													<p className='text-primary/70'>
-														{QUESTIONS_DISPLAY_TEXT[question.type]}
-													</p>
-												</div>
-
-												<div className='w-full justify-between gap-3 items-center'>
-													<p className='whitespace-normal h-10 break-all line-clamp-2'>
-														{question.text}
-													</p>
-												</div>
-											</div>
-										</Button>
-									</QuestionDialog>
-								))}
-							</div>
-
-							<QuestionDialog index={questions.length}>
-								<Button variant={'secondary'} className='w-[30%]'>Add</Button>
-							</QuestionDialog>
-						</div>
+						<QuestionsSection questions={questions} />
 					</div>
 
-					<Button className='block mx-auto mt-20' size={'lg'} type='submit'>Submit</Button>
+					<Button
+						className='block mx-auto mt-20'
+						size={'lg'}
+						type='submit'
+					>
+						Submit
+					</Button>
 				</form>
 			</Form>
 		</FormProvider>
